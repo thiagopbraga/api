1e0241382718826c119421c7e448a2cb83cf995b
Thiago Pinto Braga <thiago.braga@wyntech.inf.br>
2024-03-23 19:53:16 -0300

    remove console logs

--- src/middlewares/user-id-check.middleware.ts
+++ src/middlewares/user-id-check.middleware.ts
@@ -3,13 +3,9 @@ import { NextFunction, Request, Response } from 'express';
 
 export class UserIdCheckMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log('UserIdCheckMiddleware', 'antes');
-
     if (isNaN(Number(req.params.id)) || Number(req.params.id) <= 0) {
       throw new BadRequestException('Invalid ID');
     }
-
-    console.log('UserIdCheckMiddleware', 'depois');
 
     next();
   }
