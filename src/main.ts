ee696852208fd04e12a4dd94d60e72bbccecd20a
Thiago Pinto Braga <thiago.braga@wyntech.inf.br>
2024-03-23 21:54:15 -0300

    Enable CORS for all routes and origins

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // Enable CORS for all routes and origins
   app.useGlobalPipes(new ValidationPipe());
   // app.useGlobalInterceptors(new LogInterceptor());
   await app.listen(3000);
